8f1efa9bda9f9f8d045f5300be0bf69b248245cb
swanderz <swanson.anders@gmail.com>
2021-11-17 11:31:22 -0800

    more docs

--- dbt/include/firebolt/macros/materializations/table.sql
+++ dbt/include/firebolt/macros/materializations/table.sql
@@ -2,6 +2,14 @@
 {# Note that a nearly identical materialization appears
    in table.sql #}
   {%- set identifier = model['alias'] -%} {# alias comes from where? #}
+
+  {# Temporary workaround to issue with adapter.get_relation() until
+     the following are resolved:
+     https://github.com/firebolt-db/dbt-firebolt/issues/11
+     https://github.com/dbt-labs/dbt-core/issues/4187
+     the hack is to always drop the view/table if it already exists rather than
+     checking the db first to see if it exists.
+   #}
   {%- set target_relation = api.Relation.create(database=database,
                                                 schema=schema,
                                                 identifier=identifier,
