24aa8ae90d4c48121908d2cee5a05c3e3bb8cbb8
Stepan Burlakov <stepansergeevitch@gmail.com>
2022-01-24 15:33:10 +0200

    update setup.py

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@
 from setuptools import setup
 from setuptools.config import read_configuration
 
-config = read_configuration("setup.cfg")
-version = config["metadata"]["version"]
-dbt_version = version.rsplit(".", 1)[0]
-install_requires = [f"dbt-core~={dbt_version}", config["options"]["install_requires"]]
+config = read_configuration('setup.cfg')
+version = config['metadata']['version']
+dbt_version = version.rsplit('.', 1)[0]
+install_requires = [f'dbt-core~={dbt_version}', config['options']['install_requires']]
 
 setup(install_requires=install_requires)
