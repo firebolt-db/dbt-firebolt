49897031b6e5c0c95d0c7af2dcb3f9037670195a
ericf-firebolt <eric@firebolt.io>
2022-02-16 19:41:09 -0500

    Removed column_comment from catalog.sql.

--- dbt/include/firebolt/macros/catalog.sql
+++ dbt/include/firebolt/macros/catalog.sql
@@ -2,13 +2,15 @@
 the columns (for instance, `is_nullable` is missing) but more could be added later. #}
 
 {% macro firebolt__get_catalog(information_schemas, schemas) -%}
+  {{ log('\n\n** Information schemas:\n' ~ information_schemas, True) }}
+  {{ log('\n\n** Schemas:\n' ~ schemas, True) }}
   {%- call statement('catalog', fetch_result=True) -%}
     SELECT tbls.table_schema AS table_database
          , NULL as table_schema
+         , 'DIMENSION' AS table_type
          , tbls.table_name
          , cols.column_name
          , cols.data_type AS column_type
-         , NULL AS column_comment
     FROM "information_schema"."tables" tbls
         JOIN "information_schema"."columns" cols
             USING(table_name)
