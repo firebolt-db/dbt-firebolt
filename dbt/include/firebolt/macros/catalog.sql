cedbc78320dc442df07d7b6a9abad7fdb8de9dec
ericf-firebolt <eric@firebolt.io>
2022-03-08 17:07:29 -0500

    Cleaned up query in catalog.sql. Still not passing catalog tests in base. Putting off for now.

--- dbt/include/firebolt/macros/catalog.sql
+++ dbt/include/firebolt/macros/catalog.sql
@@ -2,52 +2,24 @@
 the columns (for instance, `is_nullable` is missing) but more could be added later. #}
 
 {% macro firebolt__get_catalog(information_schemas, schemas) -%}
-  {{ log('\n\n** Information schemas:\n' ~ information_schemas, True) }}
-  {{ log('\n\n** Schemas:\n' ~ schemas, True) }}
   {%- call statement('catalog', fetch_result=True) -%}
     SELECT tbls.table_schema AS table_database
-         , NULL as table_schema
-         , 'DIMENSION' AS table_type
+         , SPLIT_PART(tbls.table_name, '_', 1) AS table_schema
+         , tbls.table_type
          , tbls.table_name
          , cols.column_name
          , cols.data_type AS column_type
     FROM "information_schema"."tables" tbls
         JOIN "information_schema"."columns" cols
-            USING(table_name)
+        USING(table_name)
+    WHERE
+        {% for schema in schemas -%}
+          tbls.table_name ILIKE '{{ schema }}_%' {%- if not loop.last %} or {% endif -%}
+        {%- endfor %}
     ORDER BY tbls.table_name, cols.column_name
-
-    -- WITH tbls AS (
-    --     SELECT table_schema AS table_database
-    --          , NULL AS table_schema
-    --          , table_name
-    --     FROM "information_schema"."tables"
-    -- ),
-    -- cols AS (
-    --     SELECT table_name
-    --          , column_name
-    --          , data_type AS column_type
-    --          , NULL AS column_comment
-    --     FROM "information_schema"."columns"
-    -- )
-    -- SELECT tbls.table_database
-    --      , tbls.table_schema
-    --      , tbls.table_name
-    --      , cols.column_name
-    --      , cols.column_type
-    -- FROM tbls
-    -- JOIN cols ON tbls.table_name = cols.table_name
-    -- ORDER BY tbls.table_name, cols.column_name
-
   {%- endcall -%}
-  {% call statement('show_tables', fetch_result=True) %}
-      SHOW TABLES
-  {% endcall %}
   {% set results = load_result('catalog').table %}
-  {{ log('\n\n** table view\n' ~ load_result('show_tables'), True) }}
-  {{ log('\n\n** raw catalog\n' ~ load_result('catalog'), True) }}
-  {{ log('\n\n** Agate catalog\n' ~ results, True) }}
-  {{ log('\n\n** rows:\n' ~ adapter.get_rows(results), True)}}
-  {{ return(load_result('catalog').table) }}
+  {{ return(results) }}
 {%- endmacro %}
 
 
