1a0b82f606b94092cd2884a0781d101cc1d3ffa1
ericf-firebolt <eric@firebolt.io>
2022-03-11 08:54:32 -0500

    Bumped pytest value in requirements, which had regressed? Working on getting table type to work correctly. Right now it's returning None.

--- dbt/include/firebolt/macros/adapters/relation.sql
+++ dbt/include/firebolt/macros/adapters/relation.sql
@@ -2,13 +2,15 @@
   {% set target_name = adapter.quote_as_configured(to_relation.identifier, 'identifier') %}
 
   {% call statement('table_type', fetch_result=True) %}
+
     SELECT table_type FROM information_schema.tables
     WHERE table_name = '{{ from_relation }}'
   {% endcall %}
 
-  {% set table_type = load_result('table_type').table.rows.get('table_name') %}
+  {% set table_type = load_result('table_type').table %}
 
   {% call statement('rename_relation') -%}
+
     CREATE {{ table_type }} TABLE {{ from_relation }}_swap AS
       SELECT * FROM {{ from_relation }};
     CREATE {{ table_type }} TABLE {{ target_name }} AS
