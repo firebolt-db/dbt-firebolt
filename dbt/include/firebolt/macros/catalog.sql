2acfdf178485495e2fef5400423700d719302bab
ericf-firebolt <eric@firebolt.io>
2022-03-17 10:48:42 -0400

    Added a bunch of log commands.

--- dbt/include/firebolt/macros/catalog.sql
+++ dbt/include/firebolt/macros/catalog.sql
@@ -14,10 +14,12 @@ the columns (for instance, `is_nullable` is missing) but more could be added lat
          USING(table_name)
     WHERE
         {% for schema in schemas -%}
-          tbls.table_name ILIKE '{{ schema }}_%' {%- if not loop.last %} or {% endif -%}
+          tbls.table_name LIKE '{{ schema }}_%' {%- if not loop.last %} or {% endif -%}
         {%- endfor %}
     ORDER BY tbls.table_name, cols.column_name
   {%- endcall -%}
+  {{ log('\n\n** get_catalog:\n', True) }}
+  {{ log(load_result('catalog'), True) }}
   {% set results = load_result('catalog').table %}
   {{ return(results) }}
 {%- endmacro %}
