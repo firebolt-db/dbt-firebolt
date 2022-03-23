e2bed4447273cb9dd60b380bfbdf002c3834b503
ericf-firebolt <eric@firebolt.io>
2022-03-23 15:39:22 -0400

    Removed extraneous log statements and code that was commented out.

--- dbt/include/firebolt/macros/catalog.sql
+++ dbt/include/firebolt/macros/catalog.sql
@@ -10,10 +10,9 @@ the columns (for instance, `is_nullable` is missing) but more could be added lat
          , cols.column_name
          , cols.data_type AS column_type
          , 'table' AS type
-    FROM "information_schema"."tables" tbls
-        JOIN "information_schema"."columns" cols
+    FROM information_schema.tables tbls
+         JOIN information_schema.columns cols
             USING(table_name)
-    --ORDER BY tbls.table_name, cols.column_name
 
     UNION SELECT views.table_schema AS table_database
       , NULL as table_schema
@@ -22,39 +21,6 @@ the columns (for instance, `is_nullable` is missing) but more could be added lat
       , NULL AS column_name
       , NULL AS column_type
       , 'view' AS type
-    FROM "information_schema"."views" views
-
-
-    -- WITH tbls AS (
-    --     SELECT table_schema AS table_database
-    --          , NULL AS table_schema
-    --          , table_name
-    --     FROM "information_schema"."tables"
-    -- ),
-    -- cols AS (
-    --     SELECT table_name
-    --          , column_name
-    --          , data_type AS column_type
-    --          , NULL AS column_comment
-    --     FROM "information_schema"."columns"
-    -- )
-    -- SELECT tbls.table_database
-    --      , tbls.table_schema
-    --      , tbls.table_name
-    --      , cols.column_name
-    --      , cols.column_type
-    -- FROM tbls
-    -- JOIN cols ON tbls.table_name = cols.table_name
-    -- ORDER BY tbls.table_name, cols.column_name
-
-  {%- endcall -%}
-  {% call statement('show_tables', fetch_result=True) %}
-      SHOW TABLES
-  {% endcall %}
-  {% set results = load_result('catalog').table %}
-  {{ log('\n\n** table view\n' ~ load_result('show_tables'), True) }}
-  {{ log('\n\n** raw catalog\n' ~ load_result('catalog'), True) }}
-  {{ log('\n\n** Agate catalog\n' ~ results, True) }}
-  {# {{ log('\n\n** rows:\n' ~ adapter.get_rows(results), True)}} #}
+    FROM information_schema.views views
   {{ return(load_result('catalog').table) }}
 {%- endmacro %}
