f0b8d62b2a4c10d528f88f24a19ba11f9d4d2362
ericf-firebolt <eric@firebolt.io>
2022-03-25 13:58:48 -0400

    Cleaned up incremental.sql while I was waiting for a run to finish.

--- dbt/include/firebolt/macros/materializations/models/incremental/incremental.sql
+++ dbt/include/firebolt/macros/materializations/models/incremental/incremental.sql
@@ -7,22 +7,25 @@
   {% set tmp_relation = make_temp_relation(target_relation) %}
   {%- set full_refresh_mode = (should_full_refresh()) -%}
 
-  {% set on_schema_change = incremental_validate_on_schema_change(config.get('on_schema_change'),
-                                                                  default='ignore') %}
+  {% set on_schema_change = incremental_validate_on_schema_change(
+      config.get('on_schema_change'),
+      default='ignore') %}
 
   {% set tmp_identifier = model['name'] + '__dbt_tmp' %}
   {% set backup_identifier = model['name'] + "__dbt_backup" %}
 
-  {# the intermediate_ and backup_ relations should not already exist in the database;
-     get_relation will return None in that case. Otherwise, we get a relation that we can drop
-     later, before we try to use this name for the current operation. This has to happen before
-     BEGIN, in a separate transaction. #}
-  {% set preexisting_intermediate_relation = adapter.get_relation(identifier=tmp_identifier,
-                                                                  schema=schema,
-                                                                  database=database) %}
-  {% set preexisting_backup_relation = adapter.get_relation(identifier=backup_identifier,
-                                                            schema=schema,
-                                                            database=database) %}
+  {# the intermediate_ and backup_ relations should not already exist in the
+     database; get_relation will return None in that case. Otherwise, we get
+     a relation that we can drop later, before we try to use this name for the
+     current operation. This has to happen before BEGIN, in a separate transaction. #}
+  {% set preexisting_intermediate_relation = adapter.get_relation(
+                                                 identifier=tmp_identifier,
+                                                 schema=schema,
+                                                 database=database) %}
+  {% set preexisting_backup_relation = adapter.get_relation(
+                                           identifier=backup_identifier,
+                                           schema=schema,
+                                           database=database) %}
   {{ drop_relation_if_exists(preexisting_intermediate_relation) }}
   {{ drop_relation_if_exists(preexisting_backup_relation) }}
 
@@ -33,18 +36,21 @@
 
   {% set to_drop = [] %}
 
-  {# -- first check whether we want to full refresh for source view or config reasons #}
+  {# first check whether we want to full refresh for source view or config reasons #}
   {% set trigger_full_refresh = (full_refresh_mode or existing_relation.is_view) %}
 
   {% if existing_relation is none %}
-      {% set build_sql = create_view_as(target_relation, sql) %}
+      {% set build_sql = create_table_as(False, target_relation, sql) %}
   {% elif trigger_full_refresh %}
-      {#-- Make sure the backup doesn't exist so we don't encounter issues with the rename below #}
+      {# Make sure the backup doesn't exist so we don't encounter
+         issues with the rename below #}
       {# Shouldn't need these two lines, as set above.
       {% set tmp_identifier = model['name'] + '__dbt_tmp' %}
       {% set backup_identifier = model['name'] + '__dbt_backup' %} #}
-      {% set intermediate_relation = existing_relation.incorporate(path={"identifier": tmp_identifier}) %}
-      {% set backup_relation = existing_relation.incorporate(path={"identifier": backup_identifier}) %}
+      {% set intermediate_relation = existing_relation.incorporate(
+                                         path={"identifier": tmp_identifier}) %}
+      {% set backup_relation = existing_relation.incorporate(
+                                         path={"identifier": backup_identifier}) %}
 
       {% set build_sql = create_view_as(intermediate_relation, sql) %}
       {% set need_swap = true %}
@@ -54,12 +60,18 @@
     {% do adapter.expand_target_column_types(
              from_relation=tmp_relation,
              to_relation=target_relation) %}
-    {#-- Process schema changes. Returns dict of changes if successful. Use source columns for upserting/merging --#}
-    {% set dest_columns = process_schema_changes(on_schema_change, tmp_relation, existing_relation) %}
+    {#-- Process schema changes. Returns dict of changes if successful.
+         Use source columns for upserting/merging --#}
+    {% set dest_columns = process_schema_changes(on_schema_change,
+                                                 tmp_relation,
+                                                 existing_relation) %}
     {% if not dest_columns %}
       {% set dest_columns = adapter.get_columns_in_relation(existing_relation) %}
     {% endif %}
-    {% set build_sql = get_delete_insert_merge_sql(target_relation, tmp_relation, unique_key, dest_columns) %}
+    {% set build_sql = get_delete_insert_merge_sql(target_relation,
+                                                   tmp_relation,
+                                                   unique_key,
+                                                   dest_columns) %}
 
   {% endif %}
 
@@ -74,7 +86,9 @@
 
   {% do persist_docs(target_relation, model) %}
 
-  {% if existing_relation is none or existing_relation.is_view or should_full_refresh() %}
+  {% if existing_relation is none
+        or existing_relation.is_view
+        or should_full_refresh() %}
     {% do create_indexes(target_relation) %}
   {% endif %}
 
