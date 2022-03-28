c157dab9e4f07a6e7652c7d01cbfafd6db5ddd25
ericf-firebolt <eric@firebolt.io>
2022-03-28 14:58:53 -0400

    Regressed changes in catalog.sql that will instead living in passing-base-integration-test branch.

--- dbt/include/firebolt/macros/catalog.sql
+++ dbt/include/firebolt/macros/catalog.sql
@@ -3,45 +3,27 @@ the columns (for instance, `is_nullable` is missing) but more could be added lat
 
 {% macro firebolt__get_catalog(information_schemas, schemas) -%}
   {%- call statement('catalog', fetch_result=True) -%}
-    SELECT tbls.table_schema AS table_database
-         , NULL as table_schema
-         , 'DIMENSION' AS table_type
+    WITH tbls AS (
+        SELECT table_schema AS table_database
+             , NULL AS table_schema
+             , table_name
+        FROM "information_schema"."tables"
+    ),
+    cols AS (
+        SELECT table_name
+             , column_name
+             , data_type AS column_type
+             , NULL AS column_comment
+        FROM "information_schema"."columns"
+    )
+    SELECT tbls.table_database
+         , tbls.table_schema
          , tbls.table_name
          , cols.column_name
-         , cols.data_type AS column_type
-    FROM "information_schema"."tables" tbls
-        JOIN "information_schema"."columns" cols
-            USING(table_name)
+         , cols.column_type
+    FROM tbls
+    JOIN cols ON tbls.table_name = cols.table_name
     ORDER BY tbls.table_name, cols.column_name
-
-    -- WITH tbls AS (
-    --     SELECT table_schema AS table_database
-    --          , NULL AS table_schema
-    --          , table_name
-    --     FROM "information_schema"."tables"
-    -- ),
-    -- cols AS (
-    --     SELECT table_name
-    --          , column_name
-    --          , data_type AS column_type
-    --          , NULL AS column_comment
-    --     FROM "information_schema"."columns"
-    -- )
-    -- SELECT tbls.table_database
-    --      , tbls.table_schema
-    --      , tbls.table_name
-    --      , cols.column_name
-    --      , cols.column_type
-    -- FROM tbls
-    -- JOIN cols ON tbls.table_name = cols.table_name
-    -- ORDER BY tbls.table_name, cols.column_name
-
   {%- endcall -%}
-  {% call statement('show_tables', fetch_result=True) %}
-      SHOW TABLES
-  {% endcall %}
-  {% set results = load_result('catalog').table %}
   {{ return(load_result('catalog').table) }}
 {%- endmacro %}
-
-
