166341c538ab14f96c8b34f7e48ef17f893de2a7
ericf-firebolt <eric@firebolt.io>
2022-03-29 15:32:10 -0400

    Added merge.sql back in, although only get_delete_insert_merge_sql macro.

---
+++ dbt/include/firebolt/macros/materializations/models/incremental/merge.sql
@@ -0,0 +1,14 @@
+{% macro firebolt__get_delete_insert_merge_sql(target, source, unique_key, dest_columns) -%}
+    {%- set dest_cols_csv = get_quoted_csv(dest_columns | map(attribute="name")) -%}
+    {% if unique_key is not none %}
+    DELETE FROM {{ target }}
+    WHERE ({{ unique_key }}) IN (
+        SELECT ({{ unique_key }})
+        FROM {{ source }}
+    );
+    {% endif %}
+    INSERT INTO {{ target }} ({{ dest_cols_csv }})
+        SELECT {{ dest_cols_csv }}
+        FROM {{ source }}
+{%- endmacro %}
+
