007017c3eca8d2a9bbe2570c68ed29122ce4d8cc
ericf-firebolt <eric@firebolt.io>
2022-04-04 18:18:41 -0400

    Added relational to adapters.

---
+++ dbt/include/firebolt/macros/adapters/relation.sql
@@ -0,0 +1,33 @@
+{% macro firebolt__rename_relation(from_relation, to_relation) -%}
+  {# Create new relation, do CTAS to swap it, remove original relation.
+     Must check whether relation is view or table, because process is
+     different for the two. #}
+  {% set all_relations = (list_relations_without_caching(from_relation)) %}
+  {% set views = adapter.filter_table(all_relations, 'type', 'view') %}
+  {% if (views.rows | length) > 0 %}
+    {% call statement('view_definition', fetch_result=True) %}
+      SELECT view_definition FROM information_schema.views
+      WHERE table_name = '{{ from_relation.identifier }}'
+    {%- endcall %}
+    {% if load_result('view_definition')['data'] %}
+      {# For some reason this ocassionally returns an empty set.
+         In that case do nothing. Dont even ask why I have to jump through
+         the stupid hoops to get the value out of the result in the next line. #}
+      {% set view_ddl = load_result('view_definition')['data'][0][0] %}
+      {% set view_ddl = view_ddl.replace(from_relation.identifier,
+                                         to_relation.identifier) %}
+      {% call statement('new_view') %}
+        DROP VIEW {{ from_relation.identifier }};
+        {{ view_ddl }};
+      {% endcall %}
+    {% endif %}
+  {% elif (tables.rows | length) > 0 %}
+    {# There were no views, so retrieve table. (There must be a table.) #}
+    {% set table_type = adapter.filter_table(all_relations, 'table_type', '') %}
+    {% call statement('tables') %}
+      CREATE {{ table_type }} TABLE {{ to_relation.identifier }} AS
+      SELECT * FROM {{ from_relation.identifier }};
+      DROP TABLE {{ from_relation.identifier }};
+    {%- endcall %}
+  {% endif %}
+{% endmacro %}
