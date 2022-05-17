ee0d21aa25c1b51789e7891b1648ba21b651fb2f
ericf-firebolt <eric@firebolt.io>
2022-05-17 10:33:29 -0400

    Removed setup.py.

--- setup.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-from setuptools.config import setupcfg
-
-config = setupcfg('setup.cfg')
-version = config['metadata']['version']
-install_requires = [config['options']['install_requires']]
-
-dbt_version = version.rsplit('.', 1)[0]
-install_requires = [f'dbt-core~={dbt_version}', config['options']['install_requires']]
