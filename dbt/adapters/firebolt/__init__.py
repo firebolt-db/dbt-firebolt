331e5fc6a7eaba37ffe6efce4860deb3ddb225b1
ericf-firebolt <eric@firebolt.io>
2022-05-18 16:22:01 -0400

    Merged petro-fossa-fix and removed two __init__.py files that were causing import errors.

--- dbt/adapters/firebolt/__init__.py
+++ dbt/adapters/firebolt/__init__.py
@@ -1,5 +1,4 @@
 from dbt.adapters.base import AdapterPlugin
-
 from dbt.adapters.firebolt.connections import FireboltCredentials
 from dbt.adapters.firebolt.impl import FireboltAdapter
 from dbt.include import firebolt
