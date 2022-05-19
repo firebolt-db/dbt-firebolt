b5f1d50dc7ae003dd9d1e062d4dcfd46321e1224
ericf-firebolt <eric@firebolt.io>
2022-05-19 12:35:43 -0400

    Fixed issue in setup.py and setup.cfg so that it no longer uses the dbt-firebolt version to determine which version of dbt to install.

--- setup.py
+++ setup.py
@@ -4,7 +4,6 @@ from setuptools.config import read_configuration
 
 config = read_configuration('setup.cfg')
 version = config['metadata']['version']
-dbt_version = version.rsplit('.', 1)[0]
-install_requires = [f'dbt-core~={dbt_version}', config['options']['install_requires']]
+install_requires = config['options']['install_requires']
 
 setup(install_requires=install_requires)
