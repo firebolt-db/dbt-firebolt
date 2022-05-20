09b50d84b91ebc3215bc0518cbfa4de535370793
ericf-firebolt <eric@firebolt.io>
2022-05-20 10:02:04 -0400

    Began adding framework for dbt's new testing framework.

---
+++ tests/basic/test_basic.py
@@ -0,0 +1,56 @@
+from dbt.tests.adapter.basic.test_adapter_methods import BaseAdapterMethod
+from dbt.tests.adapter.basic.test_base import BaseSimpleMaterializations
+from dbt.tests.adapter.basic.test_empty import BaseEmpty
+from dbt.tests.adapter.basic.test_ephemeral import BaseEphemeral
+from dbt.tests.adapter.basic.test_generic_tests import BaseGenericTests
+from dbt.tests.adapter.basic.test_incremental import BaseIncremental
+from dbt.tests.adapter.basic.test_singular_tests import BaseSingularTests
+from dbt.tests.adapter.basic.test_singular_tests_ephemeral import (
+    BaseSingularTestsEphemeral,
+)
+from dbt.tests.adapter.basic.test_snapshot_check_cols import (
+    BaseSnapshotCheckCols,
+)
+from dbt.tests.adapter.basic.test_snapshot_timestamp import (
+    BaseSnapshotTimestamp,
+)
+
+
+class TestSimpleMaterializationsMyAdapter(BaseSimpleMaterializations):
+    pass
+
+
+class TestSingularTestsMyAdapter(BaseSingularTests):
+    pass
+
+
+class TestSingularTestsEphemeralMyAdapter(BaseSingularTestsEphemeral):
+    pass
+
+
+class TestEmptyMyAdapter(BaseEmpty):
+    pass
+
+
+class TestEphemeralMyAdapter(BaseEphemeral):
+    pass
+
+
+class TestIncrementalMyAdapter(BaseIncremental):
+    pass
+
+
+class TestGenericTestsMyAdapter(BaseGenericTests):
+    pass
+
+
+class TestSnapshotCheckColsMyAdapter(BaseSnapshotCheckCols):
+    pass
+
+
+class TestSnapshotTimestampMyAdapter(BaseSnapshotTimestamp):
+    pass
+
+
+class TestBaseAdapterMethod(BaseAdapterMethod):
+    pass
