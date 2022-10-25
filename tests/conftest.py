830bbb450e6d7ef6516f61fe337a0db278979ab3
ptiurin <petro.tiurin@firebolt.io>
2022-10-25 13:38:04 +0100

    test: Migrate to new test framework

---
+++ tests/conftest.py
@@ -0,0 +1,23 @@
+import pytest
+import os
+
+# Import the standard functional fixtures as a plugin
+# Note: fixtures with session scope need to be local
+pytest_plugins = ["dbt.tests.fixtures.project"]
+
+# The profile dictionary, used to write out profiles.yml
+# dbt will supply a unique schema per test, so we do not specify 'schema' here
+@pytest.fixture(scope="class")
+def dbt_profile_target():
+    return {
+        'type': 'firebolt',
+        'threads': 1,
+        'api_endpoint': os.getenv('API_ENDPOINT'),
+        'account_name': os.getenv('ACCOUNT_NAME'),
+        'database': os.getenv('DATABASE_NAME'),
+        'engine_name': os.getenv('ENGINE_NAME'),
+        'user': os.getenv('USER_NAME'),
+        'password': os.getenv('PASSWORD'),
+        'schema': 'test',
+        'port': 443,
+    }
---
+++ tests/functional/adapter/test_basic.py
@@ -0,0 +1,57 @@
+from pytest import mark
+
+from dbt.tests.adapter.basic.test_base import BaseSimpleMaterializations
+from dbt.tests.adapter.basic.test_singular_tests import BaseSingularTests
+from dbt.tests.adapter.basic.test_singular_tests_ephemeral import BaseSingularTestsEphemeral
+from dbt.tests.adapter.basic.test_empty import BaseEmpty
+from dbt.tests.adapter.basic.test_ephemeral import BaseEphemeral
+from dbt.tests.adapter.basic.test_incremental import BaseIncremental
+from dbt.tests.adapter.basic.test_generic_tests import BaseGenericTests
+from dbt.tests.adapter.basic.test_snapshot_check_cols import BaseSnapshotCheckCols
+from dbt.tests.adapter.basic.test_snapshot_timestamp import BaseSnapshotTimestamp
+from dbt.tests.adapter.basic.test_adapter_methods import BaseAdapterMethod
+
+@mark.skip("Requires investigation")
+class TestSimpleMaterializationsFirebolt(BaseSimpleMaterializations):
+    pass
+
+
+class TestSingularTestsFirebolt(BaseSingularTests):
+    pass
+
+
+class TestSingularTestsEphemeralFirebolt(BaseSingularTestsEphemeral):
+    pass
+
+
+class TestEmptyFirebolt(BaseEmpty):
+    pass
+
+
+@mark.skip("Requires catalog fix")
+class TestEphemeralFirebolt(BaseEphemeral):
+    pass
+
+
+@mark.skip("Requires catalog fix")
+class TestIncrementalFirebolt(BaseIncremental):
+    pass
+
+
+class TestGenericTestsFirebolt(BaseGenericTests):
+    pass
+
+
+@mark.skip("Not implemented for v1")
+class TestSnapshotCheckColsFirebolt(BaseSnapshotCheckCols):
+    pass
+
+
+@mark.skip("Not implemented for v1")
+class TestSnapshotTimestampFirebolt(BaseSnapshotTimestamp):
+    pass
+
+
+@mark.skip("Requires investigation")
+class TestBaseAdapterMethod(BaseAdapterMethod):
+    pass
