d3085a119a3eee4d17a9ce9ece52cd64e00df9ea
ptiurin <petro.tiurin@firebolt.io>
2023-01-30 21:35:24 +0000

    feat: Allow multiple threads

--- tests/conftest.py
+++ tests/conftest.py
@@ -13,7 +13,7 @@ pytest_plugins = ['dbt.tests.fixtures.project']
 def dbt_profile_target():
     return {
         'type': 'firebolt',
-        'threads': 1,
+        'threads': 2,
         'api_endpoint': os.getenv('API_ENDPOINT'),
         'account_name': os.getenv('ACCOUNT_NAME'),
         'database': os.getenv('DATABASE_NAME'),
