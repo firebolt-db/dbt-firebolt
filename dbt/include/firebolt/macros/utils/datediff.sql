64f231eff23ac14cba9225bd5d755763d73f3d51
ptiurin <petro.tiurin@firebolt.io>
2023-12-01 16:50:14 +0000

    fix macros

--- dbt/include/firebolt/macros/utils/datediff.sql
+++ dbt/include/firebolt/macros/utils/datediff.sql
@@ -1,6 +1,6 @@
 {% macro firebolt__datediff(first_date, second_date, datepart) -%}
 
-    datediff(
+    date_diff(
         '{{ datepart }}',
         {{ first_date }} :: TIMESTAMP,
         {{ second_date }} :: TIMESTAMP
