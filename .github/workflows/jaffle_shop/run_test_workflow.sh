a29a50773903cdccb630247397219571f2f71e9c
ptiurin <petro.tiurin@firebolt.io>
2024-07-26 17:07:26 +0100

    better way to test this

--- .github/workflows/jaffle_shop/run_test_workflow.sh
+++ .github/workflows/jaffle_shop/run_test_workflow.sh
@@ -4,6 +4,8 @@ dbt clean
 dbt deps
 dbt compile
 dbt run-operation stage_external_sources
+cp ../.github/workflows/jaffle_shop/sources_external_tables_id_secret.yml models/staging/sources_external_tables.yml
+dbt run-operation stage_external_sources --vars "ext_full_refresh: true"
 dbt seed
 dbt seed --full-refresh
 dbt run
