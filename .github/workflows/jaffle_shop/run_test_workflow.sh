9074742b573c4a784d6ec5efad4832b14f2905b8
Petro Tiurin <93913847+ptiurin@users.noreply.github.com>
2024-07-29 10:33:38 +0100

    feat(FIR-34997): External table id secret support (#127)

--- .github/workflows/jaffle_shop/run_test_workflow.sh
+++ .github/workflows/jaffle_shop/run_test_workflow.sh
@@ -1,9 +1,11 @@
-set -e
+set -xe
 dbt debug
 dbt clean
 dbt deps
 dbt compile
 dbt run-operation stage_external_sources
+cp ../dbt-firebolt/.github/workflows/jaffle_shop/sources_external_tables_id_secret.yml models/staging/sources_external_tables.yml
+dbt run-operation stage_external_sources --vars "ext_full_refresh: true"
 dbt seed
 dbt seed --full-refresh
 dbt run
