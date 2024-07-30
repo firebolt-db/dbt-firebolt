280bb28fbad0cc86ce73e884b714dbba4dc40261
ptiurin <petro.tiurin@firebolt.io>
2024-07-30 10:08:53 +0000

    run only on fb2.0

--- .github/workflows/jaffle_shop/run_test_workflow.sh
+++ .github/workflows/jaffle_shop/run_test_workflow.sh
@@ -6,8 +6,11 @@ dbt compile
 dbt run-operation stage_external_sources
 cp ../dbt-firebolt/.github/workflows/jaffle_shop/sources_external_tables_id_secret.yml models/staging/sources_external_tables.yml
 dbt run-operation stage_external_sources --vars "ext_full_refresh: true"
-cp ../dbt-firebolt/.github/workflows/jaffle_shop/sources_external_tables_iam.yml models/staging/sources_external_tables.yml
-dbt run-operation stage_external_sources --vars "ext_full_refresh: true"
+if [[ -n "$AWS_ACCESS_ROLE_ARN" ]]; then
+    # Can't test this on FB 1.0
+    cp ../dbt-firebolt/.github/workflows/jaffle_shop/sources_external_tables_iam.yml models/staging/sources_external_tables.yml
+    dbt run-operation stage_external_sources --vars "ext_full_refresh: true"
+fi
 dbt seed
 dbt seed --full-refresh
 dbt run
