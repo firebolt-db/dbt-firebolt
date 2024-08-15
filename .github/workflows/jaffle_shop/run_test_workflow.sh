ca1e3abbdd7e27d4772e732febd76ea616789740
Petro Tiurin <93913847+ptiurin@users.noreply.github.com>
2024-08-15 10:26:21 +0100

    feat(FIR-34986): can use COPY to create table (#131)

--- .github/workflows/jaffle_shop/run_test_workflow.sh
+++ .github/workflows/jaffle_shop/run_test_workflow.sh
@@ -10,6 +10,9 @@ if [[ -n "$AWS_ACCESS_ROLE_ARN" ]]; then
     # Can't test this on FB 1.0
     cp ../dbt-firebolt/.github/workflows/jaffle_shop/sources_external_tables_iam.yml models/staging/sources_external_tables.yml
     dbt run-operation stage_external_sources --vars "ext_full_refresh: true"
+    # Test COPY INTO
+    cp ../dbt-firebolt/.github/workflows/jaffle_shop/sources_external_tables_copy.yml models/staging/sources_external_tables.yml
+    dbt run-operation stage_external_sources --vars "ext_full_refresh: true"
 fi
 dbt seed
 dbt seed --full-refresh
