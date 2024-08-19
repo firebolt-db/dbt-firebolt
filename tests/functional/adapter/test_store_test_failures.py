a70252c3ca30ed63aee6c2ff69bdb2d050b6a719
ptiurin <petro.tiurin@firebolt.io>
2024-08-19 18:31:50 +0100

    Storing Test Failures as View tests

---
+++ tests/functional/adapter/test_store_test_failures.py
@@ -0,0 +1,32 @@
+from dbt.tests.adapter.store_test_failures_tests.basic import (
+    StoreTestFailuresAsExceptions,
+    StoreTestFailuresAsGeneric,
+    StoreTestFailuresAsInteractions,
+    StoreTestFailuresAsProjectLevelEphemeral,
+    StoreTestFailuresAsProjectLevelOff,
+    StoreTestFailuresAsProjectLevelView,
+)
+
+
+class TestStoreTestFailuresAsInteractions(StoreTestFailuresAsInteractions):
+    pass
+
+
+class TestStoreTestFailuresAsProjectLevelOff(StoreTestFailuresAsProjectLevelOff):
+    pass
+
+
+class TestStoreTestFailuresAsProjectLevelView(StoreTestFailuresAsProjectLevelView):
+    pass
+
+
+class TestStoreTestFailuresAsGeneric(StoreTestFailuresAsGeneric):
+    pass
+
+
+class TestStoreTestFailuresAsProjectLevelEphemeral(StoreTestFailuresAsProjectLevelEphemeral):
+    pass
+
+
+class TestStoreTestFailuresAsExceptions(StoreTestFailuresAsExceptions):
+    pass
