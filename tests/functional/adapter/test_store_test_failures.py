4967c480c748d420b552e3e74f8c3130401d17fb
Petro Tiurin <93913847+ptiurin@users.noreply.github.com>
2024-08-28 13:08:03 +0100

    test: Skip incompatible tests (#136)

--- tests/functional/adapter/test_store_test_failures.py
+++ tests/functional/adapter/test_store_test_failures.py
@@ -1,3 +1,5 @@
+import os
+
 from dbt.tests.adapter.store_test_failures_tests.basic import (
     StoreTestFailuresAsExceptions,
     StoreTestFailuresAsGeneric,
@@ -6,29 +8,36 @@ from dbt.tests.adapter.store_test_failures_tests.basic import (
     StoreTestFailuresAsProjectLevelOff,
     StoreTestFailuresAsProjectLevelView,
 )
+from pytest import mark
 
 
+@mark.skipif(bool(os.getenv('PASSWORD')), reason='Not supported in FB 1.0')
 class TestStoreTestFailuresAsInteractions(StoreTestFailuresAsInteractions):
     pass
 
 
+@mark.skipif(bool(os.getenv('PASSWORD')), reason='Not supported in FB 1.0')
 class TestStoreTestFailuresAsProjectLevelOff(StoreTestFailuresAsProjectLevelOff):
     pass
 
 
+@mark.skipif(bool(os.getenv('PASSWORD')), reason='Not supported in FB 1.0')
 class TestStoreTestFailuresAsProjectLevelView(StoreTestFailuresAsProjectLevelView):
     pass
 
 
+@mark.skipif(bool(os.getenv('PASSWORD')), reason='Not supported in FB 1.0')
 class TestStoreTestFailuresAsGeneric(StoreTestFailuresAsGeneric):
     pass
 
 
+@mark.skipif(bool(os.getenv('PASSWORD')), reason='Not supported in FB 1.0')
 class TestStoreTestFailuresAsProjectLevelEphemeral(
     StoreTestFailuresAsProjectLevelEphemeral
 ):
     pass
 
 
+@mark.skipif(bool(os.getenv('PASSWORD')), reason='Not supported in FB 1.0')
 class TestStoreTestFailuresAsExceptions(StoreTestFailuresAsExceptions):
     pass
