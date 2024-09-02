98e38e614ac494cd0ed9a160577867b73e227efb
Petro Tiurin <93913847+ptiurin@users.noreply.github.com>
2024-09-02 15:49:28 +0100

    test: Fix 1.0 test with quoting (#140)

--- tests/functional/adapter/unit_testing/test_unit_testing.py
+++ tests/functional/adapter/unit_testing/test_unit_testing.py
@@ -1,3 +1,5 @@
+import os
+
 from dbt.tests.adapter.unit_testing.test_case_insensitivity import (
     BaseUnitTestCaseInsensivity,
 )
@@ -20,7 +22,7 @@ class TestFireboltUnitTestingTypes(BaseUnitTestingTypes):
     @fixture
     def data_types(self):
         # sql_value, yaml_value
-        return [
+        d_types = [
             ['1', '1'],
             ["'1'", '1'],
             ['true', 'true'],
@@ -32,6 +34,13 @@ class TestFireboltUnitTestingTypes(BaseUnitTestingTypes):
                 """'{"bar": "baz", "balance": 7.77, "active": false}'""",  # json
                 """'{"bar": "baz", "balance": 7.77, "active": false}'""",
             ],
-            ["ARRAY['a','b','c']", """'{"a", "b", "c"}'"""],
             ['ARRAY[1,2,3]', """'{1, 2, 3}'"""],
         ]
+        # Firebolt 2.0 supports casting with double quotes, but not 1.0
+        if bool(os.getenv('PASSWORD')):
+            # 1.0
+            d_types.append(["ARRAY['a','b','c']", """'{''a'', ''b'', ''c''}'"""])
+        else:
+            # 2.0
+            d_types.append(["ARRAY['a','b','c']", """'{"a", "b", "c"}'"""])
+        return d_types
